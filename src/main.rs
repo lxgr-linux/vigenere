c4d5fdb361ee3b606d87adea2b1752dcf901f31a
lxgr-linux <lxgr@protonmail.com>
2021-09-24 00:30:55 +0200

    Added user input

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
+// A small program to de/en-crypt with the caesar procedure.
+//
+// Usage:
+//  - For interactive use: 
+// ```shell
+//  $ cargo run
+// ```
+//
+//  - For noninteractive use: 
+// ```shell
+//  $ cargo run <encrypt/decrypt> <key> <text>
+// ```
+//
+// by lxgr <lxgr@protonmail.com>
+
+use std::io;
+use std::io::Write;
+use std::env;
+
+
 struct Key {
-    short: String,
     long: Vec<char>,
 }
 
 impl Key {
-    fn new(short:String, word:&str) -> Key{
+    fn new(short:&str, word:&str) -> Key{
         let mut long_key = vec!();
         for i in 0..word.len() {
             long_key.push(short.chars()
                                .collect::<Vec<char>>()[i % short.len()])
         }
         Key {
-            short,
             long: long_key
         }
     }
@@ -33,7 +51,7 @@ enum Crypt{
 }
 
 fn abc() -> Vec<char>{
-    // Returns the an alphabetic list
+    // Returns an alphabetic list
     String::from("abcdefghijklmnopqrstuvwxyz")
                                 .chars().collect()
 }
@@ -88,14 +106,79 @@ fn encrypt(nums:&Vec<u32>, key:Key) -> Vec<u32>{
     gt_nums
 }
 
+fn read_from_input(text:&str) -> String{
+    // Prompts the user to input a String
+    let mut inp = String::new();
+    print!("{} ", text);
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut inp).unwrap();
+    String::from(inp.trim())
+}
+
 fn main() {
-    let word = String::from("ciqggihgcedheiihlezrrvfhdctse").trim()
-                                         .to_ascii_lowercase()
-                                         .replace(" ", "")
-                                         .to_string();
-    let mut key = Key::new(String::from("zamonien"), &word[..]);
-    key.to_anti_key();
-    let nums = string_to_nums(&word[..]);
+    let args:Vec<String> = env::args().collect();
+    if args.len() != 4 && args.len() != 1 {
+        panic!("Error: This program needs to have 3 or 0 arguments, not {}!",
+               args.len()-1);
+    }
+
+    let en:Crypt = {
+        if args.len() == 1 {
+            loop{
+                // Gets information about process
+                break match &read_from_input("[E]ncrypt or [D]ecrypt?")
+                                            .to_ascii_lowercase()[..]{
+                    "e" => Crypt::En,
+                    "d" => Crypt::De,
+                    _ => continue,
+                };
+            }
+        } else {
+            match &args[1].trim().to_ascii_lowercase()[..]{
+                "encrypt" => Crypt::En,
+                "decrypt" => Crypt::De,
+                _ => panic!("Invalid en/decryption operator")
+            }
+        }
+    };
+    
+    let mut key_list = vec!();
+    if args.len() == 1 {
+        for text in ["key word", "text"]{
+            key_list.push(
+                loop{
+                    // Gets text input, that should be decrypted 
+                    let kt = read_from_input(&format!("Enter {}:", text)[..])
+                                               .replace(" ", "")
+                                               .to_ascii_lowercase();
+                    if !all_in_abc(&kt){
+                    continue;
+                    }
+                    break kt
+                }
+            );
+        }
+    } else {
+        for i in 0..2 {
+            key_list.push(
+                {
+                    let kt = args[2 + i].replace(" ", "").to_ascii_lowercase();
+                    if !all_in_abc(&kt){
+                        panic!("The text is only allowed to contain characters and spaces")
+                        }   
+                    kt
+                }
+            );
+        }
+    };
+
+    let kt = &key_list[1][..];
+    let mut key = Key::new(&key_list[0][..], kt);
+    match en {
+        Crypt::De => key.to_anti_key(),
+        Crypt::En => {}
+    }
+    let nums = string_to_nums(kt);
     let new_nums = encrypt(&nums, key);
 
     println!("{}", nums_to_string(new_nums));
